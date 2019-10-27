ca3d2a40a2047913f758462b4b8faa4c3372f39c
senthilmaruthaiappan <senips@gmail.com>
2019-10-27 16:29:51 -0400

    circleci build fix

--- startlocaldb.sh
+++ startlocaldb.sh
@@ -1,3 +1,11 @@
+export MYSQL_DB_ADMIN_USERNAME=root
+export MYSQL_DB_ADMIN_PASSWORD=password
+export MYSQL_DB_URL=jdbc:mysql://localhost:3306
+
+export POSTGRES_DB_ADMIN_USERNAME=root
+export POSTGRES_DB_ADMIN_PASSWORD=password
+export POSTGRES_DB_URL=jdbc:postgresql://localhost:5432/
+
 mkdir -p ./data/mysqldata
 docker-compose up -d
 
